9db93e80a2c30d0e05125e9f3ff5eb577b0b6f7c
Brando Miranda <brandojazz@gmail.com>
2024-05-10 13:23:31 -0700

    verifying metamath but cant reproduce their acc, why?

--- eval_math.py
+++ eval_math.py
@@ -2,6 +2,11 @@ import argparse
 import json
 import pdb
 import jsonlines
+from pathlib import Path
+import datetime
+import wandb
+import fire
+import os
 
 import util
 from vllm import LLM, SamplingParams
@@ -51,7 +56,31 @@ def batch_data(data_list, batch_size=1):
     batch_data.append(data_list[last_start:last_end])
     return batch_data
 
-def test_hendrycks_math(model, data_path, start=0, end=MAX_INT, batch_size=1, tensor_parallel_size=1):
+# def test_hendrycks_math(model, data_path, start=0, end=MAX_INT, batch_size=1, tensor_parallel_size=1):
+def test_hendrycks_math(
+        model: str = 'mistralai/Mistral-7B-Instruct-v0.1', 
+        # data_path: str = '~/putnam-math/data/Putnam_MATH_original_static2.jsonl', 
+        data_path: str = '~/putnam-math/data/MATH_test.jsonl', 
+        start=0, end=MAX_INT, 
+        batch_size=5000, 
+        tensor_parallel_size=1,
+        mode: str = 'dryrun',  # 'dryrun' or 'online'
+        # mode: str = 'online',  # 'dryrun' or 'online'
+        ):
+    # - Start wandb run
+    path_2_eval_dataset = data_path
+    CUDA_VISIBLE_DEVICES = os.environ.get('CUDA_VISIBLE_DEVICES')
+    current_tmux_session = os.environ.get("TMUX", "").split(",")[-1]
+    today = datetime.datetime.now().strftime('%Y-m%m-d%d-t%Hh_%Mm_%Ss')
+    run_name = f'putnam-math ({today=} {model} {path_2_eval_dataset} {CUDA_VISIBLE_DEVICES=} {current_tmux_session=})'
+    run = wandb.init(mode=mode, project="putnam-math", name=run_name, save_code=True)
+    print(f'{run.url=}')
+    output_dir = Path(f'~/data/results_{today}/').expanduser() 
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # - Get eval data
+    data_path: Path = Path(data_path).expanduser()
+    print(f'{model=}\n, {data_path=}\n, {start=}\n, {end=}\n, {batch_size=}\n, {tensor_parallel_size=}\n')
     hendrycks_math_ins = []
     hendrycks_math_answers = []
     problem_prompt = (
@@ -62,9 +91,11 @@ def test_hendrycks_math(model, data_path, start=0, end=MAX_INT, batch_size=1, te
     print('promt =====', problem_prompt)
     with open(data_path, "r+", encoding="utf8") as f:
         for idx, item in enumerate(jsonlines.Reader(f)):
-            temp_instr = problem_prompt.format(instruction=item["instruction"])
+            # temp_instr = problem_prompt.format(instruction=item["instruction"])
+            temp_instr = problem_prompt.format(instruction=item["problem"])
             hendrycks_math_ins.append(temp_instr)
-            solution = item['output']
+            # solution = item['output']
+            solution = item['solution']
             temp_ans = remove_boxed(util.last_boxed_only_string(solution))
             hendrycks_math_answers.append(temp_ans)
 
@@ -74,7 +105,8 @@ def test_hendrycks_math(model, data_path, start=0, end=MAX_INT, batch_size=1, te
     print('lenght ====', len(hendrycks_math_ins))
     batch_hendrycks_math_ins = batch_data(hendrycks_math_ins, batch_size=batch_size)
 
-    stop_tokens = ["Question:", "Question", "USER:", "USER", "ASSISTANT:", "ASSISTANT", "Instruction:", "Instruction", "Response:", "Response"]
+    # stop_tokens = ["Question:", "Question", "USER:", "USER", "ASSISTANT:", "ASSISTANT", "Instruction:", "Instruction", "Response:", "Response"]
+    stop_tokens = ["Question:", "Question", "USER:", "USER", "ASSISTANT:", "ASSISTANT", "Instruction:", "Instruction", "Response:", "Response", "Problem:", "Solution:"]
     sampling_params = SamplingParams(temperature=0, top_p=1, max_tokens=2048, stop=stop_tokens)
     print('sampleing =====', sampling_params)
     llm = LLM(model=model,tensor_parallel_size=tensor_parallel_size)
@@ -100,6 +132,13 @@ def test_hendrycks_math(model, data_path, start=0, end=MAX_INT, batch_size=1, te
     print('start===', start, ', end====',end)
     print('length====', len(results), ', acc====', acc)
 
+    # - End run
+    prompt_template = prompt_template
+    prompt_gen_func = 'meta_math_TODO_what_this_is_ignore_for_now_since_they_use_their_own_thing'
+    wandb.config.update(dict(prompt_gen_func=str(prompt_gen_func), prompt_template=prompt_template, model=model, path_2_eval_dataset=path_2_eval_dataset, output_dir=output_dir))
+    print(f'{wandb.config=}')
+    run.finish()
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default='')  # model path
@@ -111,5 +150,12 @@ def parse_args():
     return parser.parse_args()
 
 if __name__ == "__main__":
-    args = parse_args()
-    test_hendrycks_math(model=args.model, data_path=args.data_file, start=args.start, end=args.end, batch_size=args.batch_size, tensor_parallel_size=args.tensor_parallel_size)
+    import time
+    start = time.time()
+    # args = parse_args()
+    # test_hendrycks_math(model=args.model, data_path=args.data_file, start=args.start, end=args.end, batch_size=args.batch_size, tensor_parallel_size=args.tensor_parallel_size)
+     
+    # test_hendrycks_math()  # python ~/MetaMath/eval_math.py
+    
+    fire.Fire(test_hendrycks_math) # python ~/MetaMath/eval_math.py 
+    print(f"Done!\a Time: {time.time()-start:.2f} sec, {(time.time()-start)/60:.2f} min, {(time.time()-start)/3600:.2f} hr\a")
